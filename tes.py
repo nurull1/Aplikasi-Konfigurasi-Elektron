7c0d1e6e5120b15b3dddd68a33b923e395d68659
nurull1 <165451803+nurull1@users.noreply.github.com>
2024-04-24 18:14:29 +0700

    Add files via upload

--- tes.py
+++ tes.py
@@ -2,15 +2,19 @@ import streamlit as st
 
 def konfigurasi_elektron(atomic_number):
     # Dictionary untuk menyimpan jumlah elektron di setiap kulit
-    electron_config = {'1': '1s¹', '2': '1s²', '3': '2s¹ 2p¹', '4': '2s² 2p²', '5': '2s² 2p³',
-                       '6': '2s² 2p⁴', '7': '2s² 2p⁵', '8': '2s² 2p⁶', '9': '2s² 2p⁶ 3s¹',
-                       '10': '2s² 2p⁶ 3s²', '11': '2s² 2p⁶ 3s² 3p¹', '12': '2s² 2p⁶ 3s² 3p²',
-                       '13': '2s² 2p⁶ 3s² 3p³', '14': '2s² 2p⁶ 3s² 3p⁴', '15': '2s² 2p⁶ 3s² 3p⁵',
-                       '16': '2s² 2p⁶ 3s² 3p⁶', '17': '2s² 2p⁶ 3s² 3p⁶ 4s¹', '18': '2s² 2p⁶ 3s² 3p⁶ 4s²'}
+    data_konfigurasi = {
+    "1": "1s¹",
+    "2": "1s²",
+    "3": "1s² 2s¹",
+    "4": "1s² 2s²",
+    "5": "1s² 2s² 2p¹",
+    # Tambahkan konfigurasi untuk unsur-unsur lain di sini...
+    "118": "1s² 2s² 2p⁶ 3s² 3p⁶ 4s² 3d¹⁰ 4p⁶ 5s² 4d¹⁰ 5p⁶ 6s² 4f¹⁴ 5d¹⁰ 6p⁶ 7s² 5f¹⁴ 6d¹⁰ 7p⁶"
+}
 
     # Mengecek apakah nomor atom valid
     if atomic_number not in electron_config:
-        return "Nomor atom tidak valid. Masukkan nomor atom antara 1 hingga 18."
+        return "Nomor atom tidak valid. Masukkan nomor atom antara 1 hingga 118."
 
     return electron_config[atomic_number]
 
@@ -25,11 +29,4 @@ def main():
     if st.button("Tampilkan Konfigurasi Elektron"):
         result = konfigurasi_elektron(atomic_number)
         st.write("Konfigurasi elektron untuk atom dengan nomor atom", atomic_number, "adalah:", result)
-
-if __name__ == "__main__":
-    main()
-
-
-
-         
 
